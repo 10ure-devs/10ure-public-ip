451a03e58b42c98fed9612547c8814cca4687d79
Malcolm Monroe <malcolm@quantgene.com>
2023-06-15 09:18:19 -0700

    Bump max retries, logic and console logging

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import { HttpClient } from "@actions/http-client";
  * @export
  */
 export async function run(): Promise<void> {
-  const maxRetries = parseInt(core.getInput("maxRetries"), 10);
+  const maxRetries = parseInt(core.getInput("maxRetries"), 20);
   const http = new HttpClient("haythem/public-ip", undefined, {
     allowRetries: true,
     maxRetries: maxRetries
@@ -16,27 +16,40 @@ export async function run(): Promise<void> {
   let numTries = 0;
   let success = false;
   while (!success && numTries < maxRetries) {
-    console.log(success);
+    console.log("retrying to fetch ip info....") 
     try {
-      const ipv4 = await http.getJson<IPResponse>(
-        "https://api.ipify.org?format=json"
-      );
-      const ipv6 = await http.getJson<IPResponse>(
+      //(Malcolm I Monroe) This url seems to be busted
+      //const ipv4 = await http.getJson<IPResponse>(
+      //  "https://api.ipify.org?format=json"
+      //);
+
+      /*
+        https://api64.ipify.org?format=json
+        returns the JSON data in the following
+        format: 
+          {"ip":"96.49.172.42"} // looks like ip4 address
+
+      */
+      const ipData = await http.getJson<IPResponse>(
         "https://api64.ipify.org?format=json"
       );
 
-      core.setOutput("ipv4", ipv4.result.ip);
-      core.setOutput("ipv6", ipv6.result.ip);
+      core.setOutput("ipv4", ipData.result.ip);
+      //core.setOutput("ipv6", ipv6.result.ip);
 
-      core.info(`ipv4: ${ipv4.result.ip}`);
-      core.info(`ipv6: ${ipv6.result.ip}`);
+      core.info(`ipv4: ${ipData.result.ip}`);
+      //core.info(`ipv6: ${ipv6.result.ip}`);
       success = true;
-      console.log("success");
+
+      if (success == true) {
+        console.log("success pulled ip", ipData.result.ip);
+      }
     } catch (error) {
+
       if (numTries == maxRetries - 1) {
         core.setFailed(error?.message);
       }
-    } 
+    }
     numTries++;
   }
 }
